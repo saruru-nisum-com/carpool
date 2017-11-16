da2e1c370e2a7e581c28f833949687b50c23af8d
nisum <nisum@192.168.2.6>
2017-11-16 13:17:02 +0530

    CPL013: Fetch the already registered Driver / Rider details: Initial code changes

---
+++ CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/UserRegistrationService.java
@@ -0,0 +1,10 @@
+package com.nisum.carpool.service.api;
+
+import com.nisum.carpool.service.dto.UserRegistrationDto;
+
+public interface UserRegistrationService {
+
+	
+	public UserRegistrationDto getUserRegistrationProfile(UserRegistrationDto dto) ;
+		
+}
