ca320267fe0db96ecc4eb8a3e7d29ca1470c1409
nisum <nisum@nisums-mbp>
2017-11-18 23:37:56 +0530

    Added createCarPooldetails method

--- CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/CarpooldetailsService.java
+++ CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/CarpooldetailsService.java
@@ -5,4 +5,6 @@ import com.nisum.carpool.service.dto.ServiceStatusDto;
 
 public interface CarpooldetailsService {
 	ServiceStatusDto updateCarpooldetails(CarpooldetailsDto carpooldetailsDto);
+	
+	ServiceStatusDto createCarPooldetails(CarpooldetailsDto carpooldetailsDto);
 }
