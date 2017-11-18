537e6763a3504efd51a05875926bf2f2c0478443
nisum <nisum@nisums-mbp>
2017-11-18 23:35:29 +0530

    Added NOTVALID and CARPOOLEXISTS statements

---
+++ CarPoolServices/carpool-data-service/src/main/java/com/nisum/carpool/data/util/Constants.java
@@ -0,0 +1,63 @@
+package com.nisum.carpool.data.util;
+
+public class Constants {
+
+	public static final String MSG_RECORD_ADD = "Record Added Successfully !!";
+	public static final String MSG_RECORD_UPDATE = "Record Updated Successfully !!";
+	public static final String MSG_RECORD_NOT_UPDATE = "Sorry Record Not Updated !!";
+	public static final String QUESTION_NOT_EXIST = "Question Details doest'n Exist !!";
+	public static final String REPLY_NOT_EXISTS = "Reply Details doest'n Exist !!";
+
+	public static final String ERROR_MESSAGE = "Error While Creating Record !!";
+	public static final String UNKNOWN_EXCEPTION = "Unknown Exception !!";
+	public static final String INTERNALSERVERERROR = "Internal Server Error !!";
+	public static final String CARPOOLDETAILS_EXISTS="Carpool Details Aready Updated";
+	
+
+	public static final String USER_UPDATED = "User updated Successfully !!";
+	public static final String USER_DELETED = "User Deleted Successfully !!";
+	public static final String USER_NOT_EXISTS = "User doesnot exists !!";
+	public static final String USERS_NOT_AVALIABLE = "Users Not Available !!";
+
+	public static final String USER_ROLE_EMPTY="UserRole Name Cannot Be Empty !!";
+	public static final String USER_ROLE_NOT_EXISTS = "User role doesnot exists !!";
+	public static final String USER_ROLE_UPDATED = "User role updated successfully !!";
+	public static final String USER_ROLE_EXISTS = "User Role Exists Already !!";
+	public static final String USER_ROLE_ADDED = "User Role Added Successfully !!";
+	public static final String USER_ROLE_DELETED = "User Role Deleted Successfully !!";
+	public static final String USER_ROLE_NOT_EXIST = "UserRole Details doest'n Exist !!";
+	public static final String USER_TYPE = "user";
+	public static final String USER_STATUS = "YES";
+
+	public static final String  USER_ROLE_CANNOTBE_SAME ="UserRole cannot be same";
+	
+	
+	
+	// Email configuration for new user account
+		public static final String GMAIL_SMTP="mail.smtp.auth";
+		
+		public static final String TRUE_FLAG="true";
+		
+		public static final String GMAIL_START_TLS="mail.smtp.starttls.enable";
+		
+		public static final String  GMAIL_HOST="mail.smtp.host";
+		
+		public static final String GMAIL_PORT="mail.smtp.port";
+		
+		public static final String  GMAIL_PORT_NUM="587";
+		
+		public static final String GMAIL_HOST_NAME="smtp.gmail.com";
+		
+		public static final String SORT_BY_ELEMENT="createdDate";
+		
+		//Notifications
+		public static final String NOTIFICATION_TYPE_QUESTION="Questionaries";
+		
+		public static final String YES="Y";
+		public static final String MSG_CARPOOL_ADD = "Carpool Added Successfully !!";
+		public static final String MSG_CARPOOL_FAILED = "Adding Carpool Failed !!";
+		
+		public static final String VALID = "valid";
+		public static final String NOTVALID = "not valid";
+		public static final String CARPOOLEXISTS = "Car Pool already exists";
+}
