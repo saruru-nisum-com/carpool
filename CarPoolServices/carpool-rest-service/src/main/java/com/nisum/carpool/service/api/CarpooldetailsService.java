1bb9ec65cbff7b11b5b1a690b5a1a924b9f8d566
Gapti001 <32867627+Gapti001@users.noreply.github.com>
2017-11-20 15:15:50 +0530

    adding Restapi for getCall

--- CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/CarpooldetailsService.java
+++ CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/CarpooldetailsService.java
@@ -1,5 +1,7 @@
 package com.nisum.carpool.service.api;
 
+import java.util.List;
+
 import com.nisum.carpool.service.dto.CarpooldetailsDto;
 import com.nisum.carpool.service.dto.ServiceStatusDto;
 
@@ -7,4 +9,6 @@ public interface CarpooldetailsService {
 	ServiceStatusDto updateCarpooldetails(CarpooldetailsDto carpooldetailsDto);
 	
 	ServiceStatusDto createCarPooldetails(CarpooldetailsDto carpooldetailsDto);
+	
+	 List<CarpooldetailsDto> getCarPoolDetails();
 }
