ebfc8542f77aae6dc48537e2ac4bb7295337a769
rprasad516 <31274359+rprasad516@users.noreply.github.com>
2017-11-24 16:10:55 -0800

    CPL-013 added test cases for rest service and dao layer

---
+++ CarPoolServices/carpool-rest-service/src/test/java/com/nisum/carpool/rest/api/RegisterRestServiceTest.java
@@ -0,0 +1,70 @@
+package com.nisum.carpool.rest.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.nisum.carpool.service.api.RegisterService;
+import com.nisum.carpool.service.dto.RegisterDTO;
+import com.nisum.carpool.service.exception.UserServiceException;
+import com.nisum.carpool.service.impl.RegisterServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RegisterRestServiceTest {
+	@InjectMocks
+	private RegisterRestService registerRestService;
+
+	@Mock
+	private RegisterServiceImpl registerServiceImpl;
+
+	/**
+	 * @author Nisum test case to fetch list of registered users from service to
+	 *         rest
+	 * @throws UserServiceException
+	 */
+	@Test
+	public void getUserProfile() throws UserServiceException {
+
+		List<RegisterDTO> regList = new ArrayList<>();
+		RegisterDTO registerDto = new RegisterDTO();
+		registerDto.setUserId("user1");
+		registerDto.setEmailNotification(true);
+		registerDto.setIsRider(1);
+		registerDto.setLocation("HYD");
+		registerDto.setLatitude("45");
+		registerDto.setLongitude("34");
+		registerDto.setMobile("1234567891");
+		registerDto.setNearby("NAMPALLY");
+		registerDto.setRegistrationId(1);
+		List<Integer> vehicleType = new ArrayList<>();
+		vehicleType.add(0, Integer.valueOf(2));
+		vehicleType.add(1, Integer.valueOf(4));
+		registerDto.setVehicleType(vehicleType);
+		regList.add(registerDto);
+
+		ResponseEntity<List<RegisterDTO>> expected = new ResponseEntity<>(regList, HttpStatus.OK);
+
+		String userId = "user1";
+		RegisterDTO regDto = new RegisterDTO();
+		regDto.setUserId(userId);
+
+		when(registerServiceImpl.getUserRegistrationProfile(regDto)).thenReturn(regList);
+
+		ResponseEntity<List<RegisterDTO>> actual = registerRestService.getUserProfile(userId);
+
+		assertEquals(expected.getStatusCode(), actual.getStatusCode());
+	}
+
+}
---
+++ CarPoolServices/carpool-rest-service/src/test/java/com/nisum/carpool/service/impl/RegisterServiceImplTest.java
@@ -0,0 +1,100 @@
+package com.nisum.carpool.service.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.nisum.carpool.data.dao.api.RegisterDAO;
+import com.nisum.carpool.data.domain.RegisterDomain;
+import com.nisum.carpool.data.repository.RegisterRepository;
+import com.nisum.carpool.service.api.RegisterService;
+import com.nisum.carpool.service.dto.RegisterDTO;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RegisterServiceImplTest {
+
+	@InjectMocks
+	private RegisterServiceImpl registerServiceImpl;
+
+	@Mock
+	private RegisterDAO registerDAO;
+
+	List<RegisterDTO> expected = null;
+
+	Timestamp mockDate = new Timestamp(System.currentTimeMillis());
+
+	/**
+	 * @author Rajendra Prasad Dava performs initialization at startup
+	 */
+	@Before
+	public void init() {
+
+		expected = new ArrayList<>();
+		RegisterDTO registerDto = new RegisterDTO();
+		registerDto.setUserId("user1");
+		registerDto.setEmailNotification(true);
+		registerDto.setCreatedDate(mockDate);
+		registerDto.setIsRider(1);
+		registerDto.setLocation("HYD");
+		registerDto.setLatitude("45");
+		registerDto.setLongitude("34");
+		registerDto.setMobile("1234567891");
+		registerDto.setModifiedDate(mockDate);
+		registerDto.setNearby("NAMPALLY");
+		registerDto.setRegistrationId(1);
+		List<Integer> vehicleType = new ArrayList<>();
+		vehicleType.add(0, Integer.valueOf(2));
+		vehicleType.add(1, Integer.valueOf(4));
+		registerDto.setVehicleType(vehicleType);
+		expected.add(registerDto);
+
+	}
+
+	/**
+	 * @author Rajendra Prasad Dava test case for fetching registered users from dao
+	 *         to service
+	 */
+	@Test
+	public void getUserRegistrationProfile() {
+
+		List<RegisterDomain> registerList = new ArrayList<>();
+		RegisterDomain registerDomain = new RegisterDomain();
+		registerDomain.setUserid("user1");
+		registerDomain.setEmailnotification(true);
+		registerDomain.setCreateddate(mockDate.toLocalDateTime());
+		registerDomain.setIsrider(1);
+		registerDomain.setLocation("HYD");
+		registerDomain.setLatitude("45");
+		registerDomain.setLongitude("34");
+		registerDomain.setMobile("1234567891");
+		registerDomain.setModifieddate(mockDate.toLocalDateTime());
+		registerDomain.setNearby("NAMPALLY");
+		registerDomain.setRegistrationid(1);
+		List<Integer> vehicleType = new ArrayList<>();
+		vehicleType.add(0, Integer.valueOf(2));
+		vehicleType.add(1, Integer.valueOf(4));
+		registerDomain.setVehicletype(vehicleType);
+
+		registerList.add(registerDomain);
+
+		RegisterDTO dto = new RegisterDTO();
+		dto.setUserId("user1");
+
+		when(registerDAO.findUserRegistrationByUserId("user1")).thenReturn(registerList);
+
+		List<RegisterDTO> actual = registerServiceImpl.getUserRegistrationProfile(dto);
+
+		assertThat(actual.size()).isEqualTo(expected.size());
+	}
+}
