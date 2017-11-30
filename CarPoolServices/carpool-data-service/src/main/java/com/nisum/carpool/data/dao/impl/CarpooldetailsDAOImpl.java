c9f7258954941b4d525676095b173cd1e0304b20
manohar <mdhavala@nisum.com>
2017-11-30 17:47:52 +0530

    Modified to remove unnecessary code

--- CarPoolServices/carpool-data-service/src/main/java/com/nisum/carpool/data/dao/impl/CarpooldetailsDAOImpl.java
+++ CarPoolServices/carpool-data-service/src/main/java/com/nisum/carpool/data/dao/impl/CarpooldetailsDAOImpl.java
@@ -85,14 +85,20 @@ public String updateCarpooldetails(Carpooldetails carpooldetails) {
 		
 		logger.info("CarpooldetailsDAOImpl: createCarpooldetails");	
 		
+		try {
+			
 			for(Carpooldetails cp:carpooldetails) {
 				carpooldetailsRepository.save(cp);
 				
 			}
 			
-			logger.info("CarpooldetailsDAOImpl: createCarpooldetails");
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+			
 		
-			return carpooldetails;
+			return carpooldetailsRepository.getCarPoolsByEmail(carpooldetails.get(0).getEmailId());
 		
 	}
 	
@@ -148,11 +154,6 @@ public String updateCarpooldetails(Carpooldetails carpooldetails) {
 			throw e;
 		}
 		return carpoolDets;
-	}
-	
-	@Override
-	public Carpooldetails getCarPoolByCpID(int cpId) {
-		return carpooldetailsRepository.getCarPoolsByCpId(cpId);
 	}
 	
 
