118f7370be754304992d6a1d6592db3585591889
Admin <psamavedam@nisum.com>
2017-12-04 12:32:26 +0530

    [Partha]:CPL019:UI Interface for Menu:Added static pages for for Shared rides, My rides, FAQ’s and My Today;s ride.

--- CarPoolWebApp/carpool-web-app/src/main/resources/static/js/carpoolRegistration/carpoolRegistration.js
+++ CarPoolWebApp/carpool-web-app/src/main/resources/static/js/carpoolRegistration/carpoolRegistration.js
@@ -12,6 +12,22 @@ var carpoolRegApp=angular.module('carpoolRegistrationApp', ['ui.router', 'gm'])
 		url:'/rider',
 		templateUrl:'./js/rider/rider.html',
 		controller: 'carpoolRegistrationController'
+	}).state('sharedRides',{
+		url:'/sharedRides',
+		templateUrl:'./js/history/sharedRides.html',
+		controller: 'carpoolRegistrationController'
+	}).state('myRides',{
+		url:'/myRides',
+		templateUrl:'./js/history/myRides.html',
+		controller: 'carpoolRegistrationController'
+	}).state('todaysRide',{
+		url:'/todaysRide',
+		templateUrl:'./js/todaysRide/todaysRide.html',
+		controller: 'carpoolRegistrationController'
+	}).state('faq',{
+		url:'/faq',
+		templateUrl:'./js/faq/faq.html',
+		controller: 'carpoolRegistrationController'
 	})
 	
 })	 
