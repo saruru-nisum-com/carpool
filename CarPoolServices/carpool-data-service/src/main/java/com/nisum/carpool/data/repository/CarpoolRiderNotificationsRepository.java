f73312aa48cb60097df3dae02ba48dc165aec533
manohar <mdhavala@nisum.com>
2017-12-05 15:34:26 +0530

    Added CarpoolRiderNotificationsRepository class

---
+++ CarPoolServices/carpool-data-service/src/main/java/com/nisum/carpool/data/repository/CarpoolRiderNotificationsRepository.java
@@ -0,0 +1,15 @@
+package com.nisum.carpool.data.repository;
+
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.nisum.carpool.data.domain.CarpoolRiderNotifications;
+
+public interface CarpoolRiderNotificationsRepository extends CassandraRepository<CarpoolRiderNotifications>{
+
+	@Query("select * from cp_carpoolridernotifications where cpid=:cpid allow filtering")
+	public List<CarpoolRiderNotifications> getNotifiedRidersByCPId(@Param("cpid") int cpid);
+}
