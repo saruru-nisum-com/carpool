bef9b2949ff58e54c4fe7de92e9b43d3459a34d4
rprasad516 <31274359+rprasad516@users.noreply.github.com>
2017-12-06 12:29:16 -0800

    Rajendra Prasad : <CPL:036>:added rider cancel and pool reopen functionality

--- CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/dto/GenericEmailDto.java
+++ CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/dto/GenericEmailDto.java
@@ -1,69 +1,197 @@
 package com.nisum.carpool.service.dto;
 
-public class GenericEmailDto {
+import java.io.Serializable;
+/**
+ * 
+ * @author Rajendra Prasad Dava :: CPL-036 : Building reusable mail component for
+ *         carpool
+ *      GenericMailDto to construct the body of mail
+ */
+public class GenericEmailDto implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String userName;
 	private String location;
 	private String date;
 	private String startTime;
 	private String returnTime;
-	private Integer status;
+	private int status;
 	private String remarks;
+	private Integer isRider;
+	private String riderUserName;
+	private boolean notifyMe;
 
+	/**
+	 * 
+	 * @return the userName
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * 
+	 * @param userName
+	 *            the userName to set
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	/**
+	 * 
+	 * @return the location
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * 
+	 * @param location
+	 *            the location to set
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
+	/**
+	 * 
+	 * @return the date
+	 */
 	public String getDate() {
 		return date;
 	}
 
+	/**
+	 * 
+	 * @param date
+	 *            the date to set
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
+	/**
+	 * 
+	 * @return teh startTime
+	 */
 	public String getStartTime() {
 		return startTime;
 	}
 
+	/**
+	 * 
+	 * @param startTime
+	 *            the startTime to set
+	 */
 	public void setStartTime(String startTime) {
 		this.startTime = startTime;
 	}
 
+	/**
+	 * 
+	 * @return the returnTime
+	 */
 	public String getReturnTime() {
 		return returnTime;
 	}
 
+	/**
+	 * 
+	 * @param returnTime
+	 *            the returnTime to set
+	 */
 	public void setReturnTime(String returnTime) {
 		this.returnTime = returnTime;
 	}
 
-	public Integer getStatus() {
+	/**
+	 * 
+	 * @return the status
+	 */
+	public int getStatus() {
 		return status;
 	}
 
-	public void setStatus(Integer status) {
+	/**
+	 * 
+	 * @param status
+	 *            the status to set
+	 */
+	public void setStatus(int status) {
 		this.status = status;
 	}
 
+	/**
+	 * 
+	 * @return the remarks
+	 */
 	public String getRemarks() {
 		return remarks;
 	}
 
+	/**
+	 * 
+	 * @param remarks
+	 *            the remarks to set
+	 */
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
+	}
+
+	/**
+	 * 
+	 * @return the isRider
+	 */
+	public Integer getIsRider() {
+		return isRider;
+	}
+
+	/**
+	 * 
+	 * @param isRider
+	 *            the isRider to set
+	 */
+	public void setIsRider(Integer isRider) {
+		this.isRider = isRider;
+	}
+
+	/**
+	 * 
+	 * @return the riderUserName
+	 */
+	public String getRiderUserName() {
+		return riderUserName;
+	}
+
+	/**
+	 * 
+	 * @param riderUserName
+	 *            the riderUserName to set
+	 */
+	public void setRiderUserName(String riderUserName) {
+		this.riderUserName = riderUserName;
+	}
+
+	/**
+	 * 
+	 * @return the notifyMe
+	 */
+	public boolean isNotifyMe() {
+		return notifyMe;
+	}
+
+	/**
+	 * 
+	 * @param notifyMe
+	 *            the notifyMe to set
+	 */
+	public void setNotifyMe(boolean notifyMe) {
+		this.notifyMe = notifyMe;
 	}
 
 	@Override
@@ -71,11 +199,14 @@ public class GenericEmailDto {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((isRider == null) ? 0 : isRider.hashCode());
 		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		result = prime * result + (notifyMe ? 1231 : 1237);
 		result = prime * result + ((remarks == null) ? 0 : remarks.hashCode());
 		result = prime * result + ((returnTime == null) ? 0 : returnTime.hashCode());
+		result = prime * result + ((riderUserName == null) ? 0 : riderUserName.hashCode());
 		result = prime * result + ((startTime == null) ? 0 : startTime.hashCode());
-		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + status;
 		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
 		return result;
 	}
@@ -94,10 +225,17 @@ public class GenericEmailDto {
 				return false;
 		} else if (!date.equals(other.date))
 			return false;
+		if (isRider == null) {
+			if (other.isRider != null)
+				return false;
+		} else if (!isRider.equals(other.isRider))
+			return false;
 		if (location == null) {
 			if (other.location != null)
 				return false;
 		} else if (!location.equals(other.location))
+			return false;
+		if (notifyMe != other.notifyMe)
 			return false;
 		if (remarks == null) {
 			if (other.remarks != null)
@@ -109,15 +247,17 @@ public class GenericEmailDto {
 				return false;
 		} else if (!returnTime.equals(other.returnTime))
 			return false;
+		if (riderUserName == null) {
+			if (other.riderUserName != null)
+				return false;
+		} else if (!riderUserName.equals(other.riderUserName))
+			return false;
 		if (startTime == null) {
 			if (other.startTime != null)
 				return false;
 		} else if (!startTime.equals(other.startTime))
 			return false;
-		if (status == null) {
-			if (other.status != null)
-				return false;
-		} else if (!status.equals(other.status))
+		if (status != other.status)
 			return false;
 		if (userName == null) {
 			if (other.userName != null)
@@ -130,7 +270,7 @@ public class GenericEmailDto {
 	@Override
 	public String toString() {
 		return "GenericEmailDto [userName=" + userName + ", location=" + location + ", date=" + date + ", startTime="
-				+ startTime + ", returnTime=" + returnTime + ", status=" + status + ", remarks=" + remarks + "]";
+				+ startTime + ", returnTime=" + returnTime + ", status=" + status + ", remarks=" + remarks
+				+ ", isRider=" + isRider + ", riderUserName=" + riderUserName + ", notifyMe=" + notifyMe + "]";
 	}
-
 }
--- CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/util/GenericMailTemplate.java
+++ CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/util/GenericMailTemplate.java
@@ -18,12 +18,15 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Component;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+import com.nisum.carpool.data.util.Pool_Status;
 import com.nisum.carpool.data.util.Ride_Status;
 import com.nisum.carpool.service.dto.EmailAccount;
 import com.nisum.carpool.service.dto.GenericEmailDto;
@@ -37,21 +40,14 @@ import com.nisum.carpool.service.dto.MessageContextDto;
  */
 @Component
 public class GenericMailTemplate {
+	Logger logger = LoggerFactory.getLogger(GenericMailTemplate.class);
 	@Autowired
 	private EmailAccount emailAccount;
 	@Autowired
 	private MessageContextDto messageContextDto;
 
-	DateFormat df = new SimpleDateFormat("dd/MM/YYYY");
-
-	/**
-	 * the method sends the mail to particular carpool user based on userMap key
-	 * 
-	 * @param ccMail
-	 * @param userMap
-	 */
 	public void sendGenericMail(String ccMail, Map<String, GenericEmailDto> userMap) {
-
+		logger.info("GenericMailTemplate::sendGenericMail::Entered into sendGenericMail functionality");
 		Properties props = new Properties();
 		props.put(Constants.GMAIL_SMTP, Constants.TRUE_FLAG);
 		props.put(Constants.GMAIL_START_TLS, Constants.TRUE_FLAG);
@@ -70,31 +66,20 @@ public class GenericMailTemplate {
 		Session session = Session.getInstance(props, authenticator);
 
 		try {
-
+			logger.info("GenericMailTemplate :: Building the mail structure");
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(fromMail));
-
 			buildMailReceipientAndBody(message, ccMail, userMap);
-
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	/**
-	 * method that builds the body of the message to be sent based on status found
-	 * in userMap
-	 * 
-	 * @param message
-	 * @param ccMail
-	 * @param userMap
-	 * @throws AddressException
-	 * @throws MessagingException
-	 */
 	public void buildMailReceipientAndBody(Message message, String ccMail, Map<String, GenericEmailDto> userMap)
 			throws AddressException, MessagingException {
+		logger.info("GenricMailTemplate :: buildMailReceipientAndBody :: Entered into buildMailReceipientAndBody");
 		Set<String> toMail = userMap.keySet();
-		String bodyMsg = null;
+		String bodyMsg;
 		for (String key : toMail) {
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(key));
 
@@ -104,40 +89,61 @@ public class GenericMailTemplate {
 			GenericEmailDto dto = userMap.get(key);
 
 			if (dto.getStatus() == Ride_Status.APPROVED.getValue()) {
+				logger.info("GenericMailTemplate :: Method to build a message when a pool is approved by driver "
+						+ messageContextDto.getApproveSub());
 				message.setSubject(messageContextDto.getApproveSub());
 				bodyMsg = messageBodyTemplate(dto, messageContextDto.getMsgPrefix(),
 						messageContextDto.getMsgApproveSuffix(), messageContextDto.getMsgWishApprove());
 				message.setText(bodyMsg);
 				message.setContent(bodyMsg, "text/html");
-
 			} else if (dto.getStatus() == Ride_Status.REJECTED.getValue()) {
+				logger.info("GenericMailTemplate :: Method to build a message when a poolis rejected by driver "
+						+ messageContextDto.getRejectSub());
+
 				message.setSubject(messageContextDto.getRejectSub());
 				bodyMsg = messageBodyTemplate(dto, messageContextDto.getMsgPrefix(),
 						messageContextDto.getMsgRejectSuffix(), messageContextDto.getMsgWish());
 				message.setText(bodyMsg);
 				message.setContent(bodyMsg, "text/html");
 
-			} else if (dto.getStatus() == Ride_Status.CANCELLED.getValue()) {
+			} else if (dto.getStatus() == Ride_Status.CANCELLED.getValue() && dto.getIsRider() == 1) {
+				logger.info("GenericMailTemplate :: Method to build a message when rider cancels the ride"
+						+ messageContextDto.getRiderCancelSub());
+				message.setSubject(messageContextDto.getRiderCancelSub());
+				bodyMsg = riderCancelMessageBodyTemplate(dto);
+				message.setText(bodyMsg);
+				message.setContent(bodyMsg, "text/html");
+			} else if (dto.getStatus() == Pool_Status.CANCELLED.getValue()) {
+				logger.info("GenericMailTemplate :: Method to build a message when driver cancels the ride"
+						+ messageContextDto.getCancelSub());
 				message.setSubject(messageContextDto.getCancelSub());
 				bodyMsg = messageBodyTemplate(dto, messageContextDto.getMsgPrefix(),
 						messageContextDto.getMsgCancelSuffix(), messageContextDto.getMsgWish());
 				message.setText(bodyMsg);
 				message.setContent(bodyMsg, "text/html");
 
+			} else if (dto.isNotifyMe() == true) {
+				logger.info(
+						"GenericMailTemplate :: Method to build a message when a pool is reopened: Pool has been reopened");
+				message.setSubject("Pool has been reopened");
+				bodyMsg = notifyMeMessageBodyTemplate(dto);
+				message.setText(bodyMsg);
+				message.setContent(bodyMsg, "text/html");
 			}
-			Transport.send(message);
 		}
+		Transport.send(message);
 
 	}
 
 	/**
-	 * builds body of message template based on status sent in userMap
+	 * method that builds message template for approve, reject, driver cancel by
+	 * taking following parameters
 	 * 
 	 * @param genericEmailDto
 	 * @param msgPrefix
 	 * @param msgSuffix
 	 * @param wishMsg
-	 * @return
+	 * @return message that to be set for body of email
 	 */
 	public String messageBodyTemplate(GenericEmailDto genericEmailDto, String msgPrefix, String msgSuffix,
 			String wishMsg) {
@@ -182,6 +188,83 @@ public class GenericMailTemplate {
 		sb.append(
 				"<i style='font-size:10px;font-family:Trebuchet MS;padding:10px'>Note: This is an auto generated e-mail. Please check this in <a href='#'>Carpool</a>.</i>");
 		return sb.toString();
+	}
 
+	/**
+	 * method that builds mail template when rider cancels the ride
+	 * 
+	 * @param genericEmailDto
+	 * @return message that to be set for body of email
+	 */
+	public String riderCancelMessageBodyTemplate(GenericEmailDto genericEmailDto) {
+
+		StringBuilder sb = new StringBuilder();
+
+		String headerContent = "";
+		String footerContent = "";
+
+		try {
+			headerContent = Files.toString(new File(messageContextDto.getHeaderPath()), Charsets.UTF_8);
+			footerContent = Files.toString(new File(messageContextDto.getFooterpath()), Charsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		logger.info(
+				"GenericMailTemplate :: riderCancelMessageBodyTemplate::Building body of mail when rider cancels the ride");
+		sb.append(headerContent);
+		sb.append("<br/>");
+		sb.append("<html><head></head><title></title>");
+		sb.append("<body style='font-size:12px;font-family:Trebuchet MS;padding:10px'>");
+		sb.append("<i>Hi" + "  " + genericEmailDto.getUserName() + "," + "</i>");
+		sb.append("<br><br>");
+
+		sb.append("<b style='font-size:14px;font-family:Trebuchet MS;padding:10px'>"
+				+ genericEmailDto.getRiderUserName() + " has cancelled his ride on " + genericEmailDto.getDate()
+				+ " from " + genericEmailDto.getLocation() + " to Nisum at " + genericEmailDto.getStartTime()
+				+ " and from Nisum to " + genericEmailDto.getLocation() + " at  " + genericEmailDto.getReturnTime()
+				+ ".</b>");
+		sb.append("<br><br><br>");
+		sb.append("<i>Thanks & Regards,</i>");
+		sb.append("<br>");
+		sb.append("<font color=red><i>Carpool Application.</i></font><hr/></html>");
+		sb.append(footerContent);
+		sb.append(
+				"<i style='font-size:10px;font-family:Trebuchet MS;padding:10px'>Note: This is an auto generated e-mail. Please check this in <a href='#'>Carpool</a>.</i>");
+		return sb.toString();
+	}
+
+	public String notifyMeMessageBodyTemplate(GenericEmailDto genericEmailDto) {
+
+		StringBuilder sb = new StringBuilder();
+
+		String headerContent = "";
+		String footerContent = "";
+
+		try {
+			headerContent = Files.toString(new File(messageContextDto.getHeaderPath()), Charsets.UTF_8);
+			footerContent = Files.toString(new File(messageContextDto.getFooterpath()), Charsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		logger.info("GenericMailTemplate :: notifyMeMessageBodyTemplate::Building body of mail for pool reopened");
+		sb.append(headerContent);
+		sb.append("<br/>");
+		sb.append("<html><head></head><title></title>");
+		sb.append("<body style='font-size:12px;font-family:Trebuchet MS;padding:10px'>");
+		sb.append("<i>Hi" + "  " + genericEmailDto.getUserName() + "," + "</i>");
+		sb.append("<br><br>");
+		sb.append("<b style='font-size:14px;font-family:Trebuchet MS;padding:10px'> A ride  on "
+				+ genericEmailDto.getDate() + " from " + genericEmailDto.getLocation() + " to Nisum at "
+				+ genericEmailDto.getStartTime() + " and from Nisum to " + genericEmailDto.getLocation() + " at  "
+				+ genericEmailDto.getReturnTime() + " has been reopened</b>");
+		sb.append("<br><br><br>");
+		sb.append("<i>Thanks & Regards,</i>");
+		sb.append("<br>");
+		sb.append("<font color=red><i>Carpool Application.</i></font><hr/></html>");
+		sb.append(footerContent);
+		sb.append(
+				"<i style='font-size:10px;font-family:Trebuchet MS;padding:10px'>Note: This is an auto generated e-mail. Please check this in <a href='#'>Carpool</a>.</i>");
+
+		return sb.toString();
 	}
 }
