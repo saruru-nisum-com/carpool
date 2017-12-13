e5f92b966a2b2c1bf1d61a4fbc62b34437ca33e1
manohar <mdhavala@nisum.com>
2017-12-13 16:34:49 +0530

    Changed return type

--- CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/CarpoolRiderDetailsService.java
+++ CarPoolServices/carpool-rest-service/src/main/java/com/nisum/carpool/service/api/CarpoolRiderDetailsService.java
@@ -3,12 +3,15 @@ package com.nisum.carpool.service.api;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
+
 import com.nisum.carpool.data.domain.CarpoolRiderDetails;
 import com.nisum.carpool.service.dto.CarpoolRiderDetailsDTO;
 import com.nisum.carpool.service.dto.CarpoolRiderOptedDetailsDto;
 import com.nisum.carpool.service.dto.RiderBookingDetailsDTO;
 import com.nisum.carpool.service.dto.RiderStatusDTO;
 import com.nisum.carpool.service.dto.ServiceStatusDto;
+
 
 public interface CarpoolRiderDetailsService {
 
@@ -24,7 +27,7 @@ public interface CarpoolRiderDetailsService {
 
 	List<RiderBookingDetailsDTO> getRiderBookingDetails(String emailId); 
 
-	List<CarpoolRiderDetailsDTO> cancelRiderBookingdetails(List<CarpoolRiderDetailsDTO> rides) throws Exception; 
+	ResponseEntity<?> cancelRiderBookingdetails(List<RiderBookingDetailsDTO> rides);  
 
 	Boolean updateRiderStatus(List<RiderStatusDTO> riderStatusDtoListObj) throws Exception;
 	
